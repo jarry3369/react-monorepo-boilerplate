95ef7d3241092048b5b64ea84f43f0125e5215f6
jarry3369 <jarry3369@gmail.com>
2023-11-28 10:37:07 +0900

    default css setup

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import FileRoutes from "@core/route";
+
+import "./index.css";
+
 /**
  * 수정 금지
  */
